60c92b733a990545b9b6967d213caf30d41737a8
Mahavir-2003 <patelmahavir2003@outlook.com>
2024-03-29 21:08:17 +0530

    Add new packages and update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:v4/screens/Home/home.dart';
+import 'package:v4/screens/translate/translate.dart';
+import 'package:v4/screens/weather/weather.dart';
+import 'package:v4/screens/hospital/hospital.dart';
+import 'package:v4/screens/emergency/emergency.dart';
+import 'package:v4/screens/yatri/yatri.dart';
+import 'package:v4/screens/explore/explore.dart';
+import 'package:v4/screens/feedback/feedback.dart';
+import 'package:v4/screens/auth/auth.dart';
+import 'package:v4/controllers/location_controller.dart';
+import 'package:v4/screens/auth/api/auth_api.dart';
+import 'package:v4/screens/auth/pages/login_page.dart';
+import 'package:v4/screens/auth/pages/register_page.dart';
+import 'package:v4/screens/auth/pages/account_page.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: ((context) => AuthAPI()),
+      child: const MyApp(),
+    ),
+  );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    Get.put(LocationController());
+    final value = context.watch<AuthAPI>().status;
+    print('TOP CHANGE Value changed to: $value!');
+
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xff0E1219),
+        textTheme: GoogleFonts.interTextTheme(
+          Theme.of(context).textTheme,
+        ).copyWith(
+            bodySmall: GoogleFonts.syne(
+              textStyle: Theme.of(context).textTheme.bodySmall,
+              color: Colors.white,
+              fontWeight: FontWeight.w600,
+              fontSize: 22,
+            ),
+            displayMedium: GoogleFonts.inter(
+              textStyle: Theme.of(context).textTheme.displayMedium,
+              fontWeight: FontWeight.w700,
+              fontSize: 18,
+              color: Colors.white,
+            ),
+            displaySmall: GoogleFonts.inter(
+              textStyle: Theme.of(context).textTheme.displaySmall,
+              fontWeight: FontWeight.w400,
+              fontSize: 16,
+              color: Colors.white,
+            )),
       ),
+      debugShowCheckedModeBanner: false,
+      title: 'v4',
+      initialRoute: "/",
+      routes: {
+        '/loading': (context) => const Scaffold(
+              body: Center(child: CircularProgressIndicator()),
+            ),
+        '/': (context) => const Home(),
+        '/auth': (context) => const Auth(),
+        '/translate': (context) => const Translate(),
+        '/weather': (context) => const Weather(
+              location: "Mehsana",
+            ),
+        '/hospital': (context) => const Hospital(),
+        '/emergency': (context) => const Emergency(),
+        '/yatri': (context) => const Yatri(),
+        '/explore': (context) => const Explore(),
+        '/login': (context) => const LoginPage(),
+        '/feedback': (context) => const FeedbackForm(),
+        '/register': (context) => const RegisterPage(),
+        '/account': (context) => const AccountPage(),
+      },
     );
   }
 }
